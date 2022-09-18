d6f40edc42000b3f524bcd9b3da8db86f6776988
Jerrorld <jerrorldk@gmail.com>
2022-09-18 11:30:23 -0700

    navbar with style

--- views/places/home.jsx
+++ views/places/home.jsx
@@ -7,7 +7,7 @@ function home () {
             <main>
                 <h1>HOME</h1>
                 <div>
-                    <img src='/images/goodeats-yqr-DRZHzm-uAaM-unsplash.jpg' alt='bowl of noodles'></img>
+                    <img src='/images/goodeats-yqr-DRZHzm-uAaM-unsplash.jpg' alt='bowl of noodles' width="80%"></img>
                     <div>
                     Photo by <a href="https://unsplash.com/@goodeats_yqr?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">GoodEats YQR</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                     </div>
