e1682686d7be4017c9adc686c19f14334570ae32
Jerrorld <jerrorldk@gmail.com>
2022-09-26 16:50:35 -0700

    styled the show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,31 +5,31 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{data.place.name}</h1>
-            <img src={data.place.pic} alt={data.place.name} width="40%"></img>
-            <div>
-                <h2>Rating</h2>
-                {/* add rating */}
-                <p>currently unrated</p>
+            <div className='row'>
+                <div className='col-sm-6'>
+                    <img src={data.place.pic} alt={data.place.name} width="40%"></img>
+                        <h3>Located in {data.place.city}, {data.place.state}</h3>
+                </div>
+                <div className='col-sm-6'>
+                    <h1>{data.place.name}</h1>
+                        <h2>Rating</h2>
+                            <p>Not Rated</p>
+                        <h2>Description</h2>
+                            <p>{data.place.showEstablished()}<br></br>
+                            Serving {data.place.cuisines}</p>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                    Edit
+                </a> 
+                <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                    <button type='submit' className='btn btn-danger'>
+                        Delete
+                    </button>
+                </form>
+                </div>
+                {/* Comments */}
+                <br/>
+                <h2>Comments</h2>
             </div>
-            <h2>Description</h2>
-            <p>{data.place.showEstablished()}<br></br>
-            Serving {data.place.cuisines}</p>
-
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                Edit
-            </a> 
-            <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
-                <button type='submit' className='btn btn-danger'>
-                    Delete
-                </button>
-            </form>
-
-
-            {/* Comments */}
-            <br/>
-            <h2>Comments</h2>
-            
           </main>
         </Def>
     )
