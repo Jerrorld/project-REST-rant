7d0055dbcf0ded2a63670e7f84c3a06d58c3215c
Jerrorld <jerrorldk@gmail.com>
2022-10-01 22:12:21 -0700

    updated the edit and show pages

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -55,16 +55,16 @@ function show (data) {
                   <h2>Description</h2>
                     <p>{data.place.showEstablished()}<br></br>
                       Serving {data.place.cuisines}</p>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
                     Edit
                 </a> 
-                <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                <form method='POST' action={`/places/${data.place.id}?_method=DELETE`}>
                     <button type='submit' className='btn btn-danger'>
                         Delete
                     </button>
                 </form>
                 </div>
-              
+
                 <hr/>
                 <h2>Comments</h2>
                   {comments}
